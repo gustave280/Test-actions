b2dbfc7f29ab91c33dd1eff2bebfa341793bb91d
gustave1991 <gustaveilouga@gmail.com>
2023-09-23 08:25:07 -0700

    terraform workflow

---
+++ .github/workflows/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = us-east-1
+
+  
+}
+
+resource "aws_vpc" "test" {
+    cidr_block = "10.0.0.0/26"
+      tags = {
+        name= "test-vpc"
+      }
+        
+        
+
+  
+}
